10296b0c4c6763aa3b914bfd8261d93e80b83a9d
TPrep70532 <TPrep70532@outlook.com>
2018-11-10 16:57:11 -0500

    Commiting initial changes

---
+++ RB70532LabTestApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RB70532LabTestApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
